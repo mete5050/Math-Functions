6b0e4d25d65f225ecbb2cb085bf89e72d41e881b
mete5050 <33295066+mete5050@users.noreply.github.com>
2018-10-21 21:42:52 +0300

    Create kombinasyon.py

---
+++ kombinasyon.py
@@ -0,0 +1,22 @@
+print("(n)")
+print("(r)")
+
+n=int(input("n= "))
+r=int(input("r= "))
+x=0
+n1=1
+r1=1
+r2=r
+while(x<r2):
+    n1=n*n1
+
+    n-=1
+    x+=1
+x=0    
+
+while(x<r2):
+    r1=r*r1
+    r-=1
+    x+=1
+
+print(int(n1/r1))
